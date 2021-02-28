66302ec95c95639c4f3da29111068644b79edb6d
sunyuzero <755174012@qq.com>
2021-02-28 20:37:15 +0800

    homework1

---
+++ Homework1/calculater-ui/calculater-ui/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calculater_ui
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        double num1, num2 , result;
+        char operation;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            result = num1 / num2;
+            operation = '/';
+        }
+
+     
+
+     
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            result = num1 - num2;
+            operation = '-';
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            result = num1 * num2;
+            operation = '*';
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            label3.Text = num1.ToString() + operation.ToString() + num2.ToString() + "=" + result.ToString();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            num1 = double.Parse(textBox1.Text);
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            num2 = double.Parse(textBox2.Text);
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            result = num1 + num2;
+            operation = '+';
+        }     
+
+    }
+}
---
+++ Homework1/calculater/calculater/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1, num2;
+            Console.Write("请输入两个操作数： ");
+            String[] strs = Console.ReadLine().Split(' ');
+            num1 = double.Parse(strs[0]);
+            num2 = double.Parse(strs[1]);
+            Console.Write("请输入操作符：");
+            char ch = char.Parse(Console.ReadLine());      
+            switch (ch)
+            {
+                case '+':
+                    Console.WriteLine("结果为：{0}", num1 + num2);
+                    break;
+                case '-':
+                    Console.WriteLine("结果为：{0}", num1 - num2);
+                    break;
+                case '*':
+                    Console.WriteLine("结果为：{0}", num1 * num2);
+                    break;
+                case '/':
+                    Console.WriteLine("结果为：{0}", num1 / num2);
+                    break;
+                default:
+                    Console.WriteLine("不支持此操作符");
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
